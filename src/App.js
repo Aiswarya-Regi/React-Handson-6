acab1af05182c490cf82cfd9e72027a4ec269641
Aiswarya <aiswaryaregi69@gmail.com>
2023-01-29 19:41:30 +0530

    Handson 6

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Components/Navbar';
+import RouterFile from './Components/RouterFile';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <RouterFile/>
+    </div>
+  );
+}
+
+export default App;
